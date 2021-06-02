1a767b0afffc38f8294e1e2d6b1df8618f1a7400
Maria Nathaly Gomes de Mesquita <Maria.Mesquita@ativainvestimentos.com.br>
2021-06-02 14:46:29 -0300

    feat(): Criação do projeto base

---
+++ ByteBank.ImportacaoExportacao/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using ByteBank.ImportacaoExportacao.Modelos;
+
+namespace ByteBank.ImportacaoExportacao
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+        
+    }
+}
