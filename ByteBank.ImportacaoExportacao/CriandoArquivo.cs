850ab09d05b964828ed527ce30cd27d6be77c2d9
Maria Nathaly Gomes de Mesquita <Maria.Mesquita@ativainvestimentos.com.br>
2021-06-02 14:49:22 -0300

    feat(CriandoArquivo): Fazendo parse e StreamWriter

---
+++ ByteBank.ImportacaoExportacao/CriandoArquivo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.ImportacaoExportacao
+{
+    partial class Program
+    {
+        static void CriarArquivo()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            {
+                var contaComoString = "4371,10886,1000000.50,Nathaly Mesquita";
+
+                var encoding = Encoding.UTF8;
+                var bytes = encoding.GetBytes(contaComoString);
+
+                fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
