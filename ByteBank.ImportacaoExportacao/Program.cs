1e57503831a5bdacdd5accaab355800dd14a9672
Maria Nathaly Gomes de Mesquita <Maria.Mesquita@ativainvestimentos.com.br>
2021-06-03 00:41:51 -0300

    fix(): Auxiliares na classe File

--- ByteBank.ImportacaoExportacao/Program.cs
+++ ByteBank.ImportacaoExportacao/Program.cs
@@ -12,6 +12,19 @@ namespace ByteBank.ImportacaoExportacao
     {
         static void Main(string[] args)
         {
+            File.WriteAllText("escrevendoComAClasseFile.txt","Testando File.WriteAllText");
+            Console.WriteLine("Arquivo escrevendoComAClasseFile criado!");
+
+            var bytesArquivo = File.ReadAllBytes("contas.txt");
+            Console.WriteLine($"Aquivo contas.txt possui{bytesArquivo.Length} bytes");
+
+            var linhas = File.ReadAllLines("contas.txt");
+            Console.WriteLine(linhas.Length);
+            foreach (var linha in linhas)
+            {
+                Console.WriteLine(linha);
+            }
+
             Console.ReadLine();
         }
         
